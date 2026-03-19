37e6e83fd95c795cbcac8c977a7a7b5e90ccbe49
suma <suma@gmail.com>
2026-03-19 06:16:01 +0530

    pop up message added

--- script.js
+++ script.js
@@ -7,38 +7,19 @@ function addlist(){
     }
     else{
         let li = document.createElement("li");
+        li.appendChild(document.createTextNode(IB.value));
 
-        // ⏰ Get time (HH:MM only)
-        let now = new Date();
-        let hours = now.getHours();
-        let minutes = now.getMinutes();
-
-        hours = hours < 10 ? '0' + hours : hours;
-        minutes = minutes < 10 ? '0' + minutes : minutes;
-
-        let time = hours + ":" + minutes;
-
-      
-        let textNode = document.createTextNode(IB.value + " ");
-        li.appendChild(textNode);
-
- 
-        let timeSpan = document.createElement("small");
-        timeSpan.innerHTML = "(" + time + ")";
-        timeSpan.className = "time";
-        li.appendChild(timeSpan);
-
-     
+        
         let edit = document.createElement("span");
-        edit.innerHTML = " 🖋️";
+        edit.innerHTML = " ✏️";
         edit.className = "edit";
         li.appendChild(edit);
 
        
-        let del = document.createElement("span");
-        del.innerHTML = "\u00d7";
-        del.className = "delete";
-        li.appendChild(del);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        span.className = "delete";
+        li.appendChild(span);
 
         LC.appendChild(li);
     }
@@ -48,7 +29,7 @@ function addlist(){
 
 LC.addEventListener("click", function(e){
 
-   
+  
     if(e.target.tagName === "LI"){
         e.target.classList.toggle("checked");
 
@@ -59,21 +40,21 @@ LC.addEventListener("click", function(e){
         saveinfo();
     }
 
-
+   
     else if(e.target.className === "delete"){
         e.target.parentElement.remove();
         saveinfo();
     }
 
+  
     else if(e.target.className === "edit"){
         let li = e.target.parentElement;
-
-        let taskText = li.childNodes[0].nodeValue.trim();
+        let taskText = li.childNodes[0].nodeValue;
 
         let updatedTask = prompt("Edit your task:", taskText);
 
         if(updatedTask !== null && updatedTask.trim() !== ""){
-            li.childNodes[0].nodeValue = updatedTask + " ";
+            li.childNodes[0].nodeValue = updatedTask;
             saveinfo();
         }
     }
